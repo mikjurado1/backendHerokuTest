55d9d2798251ad901ae96762222912e02b38522e
Michael Jurado <mikjurado1@gmail.com>
2023-12-13 09:52:05 +0100

    adaptado a mysql

--- src/main/java/com/javatechie/springbootazuresql/Employee.java
+++ src/main/java/com/javatechie/springbootazuresql/Employee.java
@@ -2,6 +2,7 @@ package com.javatechie.springbootazuresql;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -15,7 +16,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class Employee {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
     private String dept;
